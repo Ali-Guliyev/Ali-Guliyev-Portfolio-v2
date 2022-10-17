e17acc2d346fa42d9ed46d4bdb3da3d06f15536a
Ali Guliyev ⚡ <54935778+Ali-Guliyev@users.noreply.github.com>
2022-10-17 16:22:56 +0300

    changed header

--- components/header.vue
+++ components/header.vue
@@ -6,81 +6,104 @@ import {
   randomFloatNumber,
 } from "../composables/utilities";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const rowsAmount = ref(0);
+const digitAmount = 30;
 
 onMounted(() => {
   // * Header Terminal Hacking Animation
   const terminalTypingArea = document.querySelector(".terminal .typing-area");
-  const digitAmount = 30;
-  const rowsAmount = 20;
-  // Generate ROWS
-  for (let i = 0; i < rowsAmount; i++) {
-    let rowEl = document.createElement("div");
-    rowEl.classList.add("row");
-    // Generate DIGITS
-    for (let j = 0; j < digitAmount; j++) {
-      rowEl.innerHTML += `<span>${randomArrItem([0, 1])}</span>`;
-      rowEl.style.display = "flex";
-      rowEl.style.flexDirection = "column";
-    }
-    terminalTypingArea.appendChild(rowEl);
-  }
-  // * Terminal hacking animation
-  const terminalRows = document.querySelectorAll(".terminal .row");
-  let counterArr = [];
-  let addingTimeArr = [];
-  for (let i = 0; i < rowsAmount; i++) {
-    counterArr.push(randomFloatNumber(0, 5));
-  }
-  terminalRows.forEach((row, rowIndex) => {
-    row.childNodes.forEach((digit) => {
-      counterArr[rowIndex] += 0.1;
-      digit.style.animation = `fade-out 1s ease-in-out ${counterArr[rowIndex]}s infinite`;
-      setTimeout(() => {
-        digit.style.opacity = "1";
-      }, counterArr[rowIndex] * 1000);
-    });
+
+  gsap.to(terminalTypingArea, {
+    opacity: 1,
+    delay: 1,
+    duration: 1,
   });
+
+  const changeTerminalRowsForWindowWidth = () => {
+    if (window.innerWidth < 500) {
+      rowsAmount.value = 10;
+    } else {
+      rowsAmount.value = 20;
+    }
+  };
+
+  changeTerminalRowsForWindowWidth();
+
+  // Generate ROWS and DIGITS inside of a temrinal
+  const createTerminalContent = () => {
+    terminalTypingArea.innerHTML = "";
+
+    // Generate ROWS
+    for (let i = 0; i < rowsAmount.value; i++) {
+      let rowEl = document.createElement("div");
+      rowEl.classList.add("row");
+      // Generate DIGITS
+      for (let j = 0; j < digitAmount; j++) {
+        rowEl.innerHTML += `<span>${randomArrItem([0, 1])}</span>`;
+        rowEl.style.display = "flex";
+        rowEl.style.flexDirection = "column";
+      }
+      terminalTypingArea.appendChild(rowEl);
+    }
+  };
+
+  createTerminalContent();
+
+  // * Terminal hacking animation
+  const animateTerminal = () => {
+    const terminalRows = document.querySelectorAll(".terminal .row");
+    let counterArr = [];
+    for (let i = 0; i < rowsAmount.value; i++) {
+      counterArr.push(randomFloatNumber(0, 1));
+    }
+    terminalRows.forEach((row, rowIndex) => {
+      row.childNodes.forEach((digit) => {
+        counterArr[rowIndex] += 0.08;
+        digit.style.animation = `fade-out 1s ease-in-out ${counterArr[rowIndex]}s infinite`;
+      });
+    });
+  };
+
+  animateTerminal();
+
   // // * Header typing animation
-  const skillsWords = [
+  const skillWords = [
     "creating websites",
     "making games",
-    "solving hard algorithmic questions.",
+    "solving hard algorithmic questions",
   ];
-  const typingArea = document.querySelector("header .typingArea");
-  const cursor = document.querySelector("header .cursor");
-  const addingTime = 100;
-  const typedWordsAmount = 1000;
-  let typingTime = addingTime;
-  typingTime += 1000;
-  for (let j = 0; j < typedWordsAmount; j++) {
-    skillsWords.forEach((word, index) => {
-      // Writing letters
-      for (let i = 0; i < word.length; i++) {
+  const typingArea = document.querySelector(".typingContainer .second-text");
+  const additionTime = 100;
+  let typingTime = 800;
+  const timesRepeated = 1000;
+
+  for (let i = 0; i < timesRepeated; i++) {
+    skillWords.forEach((word) => {
+      for (let letter of word) {
         setTimeout(() => {
-          typingArea.textContent += word[i];
+          typingArea.textContent += letter;
         }, typingTime);
-        typingTime += addingTime;
+        typingTime += additionTime;
       }
-      typingTime += 2000;
-      // Removing letters
-      for (let i = word.length; i > 0; i--) {
+
+      typingTime += 500;
+
+      for (let j = word.length; j > 0; j--) {
         setTimeout(() => {
           typingArea.textContent = removeLastEl(typingArea.textContent);
         }, typingTime);
-        typingTime += addingTime;
+        typingTime += additionTime;
       }
     });
   }
-  // * GSAP Animations
-  const contLeft = document.querySelector(".container .left");
-  const contRightTerminal = document.querySelector(".container .right");
-  gsap.registerPlugin(ScrollTrigger);
-  gsap.from(contLeft, {
-    duration: 1,
-    x: "-50px",
-    opacity: 0,
-    scrollTrigger: contLeft,
+
+  // Check if resized
+  window.addEventListener("resize", () => {
+    changeTerminalRowsForWindowWidth();
+
+    createTerminalContent();
+    animateTerminal();
   });
 });
 </script>
@@ -90,10 +113,11 @@ onMounted(() => {
     <div class="container">
       <div class="left">
         <h1>Ali Guliyev</h1>
-        <p>
-          I love <span class="typingArea"></span>
-          <span class="cursor"></span>
-        </p>
+        <div class="typingContainer">
+          <span class="text first-text">I love</span>
+          <span class="text second-text"></span>
+          <span class="cursor">│</span>
+        </div>
       </div>
 
       <div class="right terminal">
@@ -115,8 +139,8 @@ onMounted(() => {
 @import "../assets/scss/variants";
 
 header {
-  position: relative;
   @include flex();
+  position: relative;
   padding: 0 120px;
 
   .container {
@@ -129,7 +153,7 @@ header {
     .right.terminal {
       max-width: 900px;
       width: 100%;
-      max-height: 500px;
+      height: 500px;
       border: 1px solid black;
       background-color: rgba(19, 19, 19, 0.75);
       border-radius: 25px;
@@ -138,18 +162,13 @@ header {
       .typing-area {
         font-weight: bolder;
         height: 100%;
+        opacity: 0;
         @include flex(space-evenly, start);
-
-        .row {
-          span {
-            opacity: 0;
-          }
-        }
       }
 
       .top {
         @include flex(space-between);
-        background-color: rgb(15, 15, 15);
+        background-color: #0f0f0f;
         padding: 10px 20px;
 
         .buttons {
@@ -179,41 +198,75 @@ header {
         font-size: 45px;
       }
 
-      p {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-
-        .typingArea {
-          display: flex;
-          margin-left: 6px;
+      .typingContainer {
+        .text {
+          position: relative;
         }
 
-        .cursor {
-          height: 20px;
-          width: 2px;
-          margin-left: 5px;
-          background-color: $green;
-          animation: fade-out 0.3s ease-in-out infinite alternate;
+        .first-text {
+          margin-right: 6px;
+        }
+
+        .second-text {
+          font-size: 17px;
         }
       }
     }
   }
 }
 
-@media screen and (max-width: 1200px) {
-  header .container {
-    flex-direction: column;
-    text-align: center;
+@media screen and (max-width: 500px) {
+  #home {
+    padding: 0 18px !important;
+  }
+}
 
+@media screen and (max-width: 900px) {
+  #home {
+    padding: 0 30px;
+  }
+}
+
+@media screen and (max-width: 1920px) {
+  #home {
     .left {
-      margin-bottom: 30px;
+      h1 {
+        font-size: 35px;
+      }
     }
 
     .right.terminal {
-      min-width: 300px;
-      height: 400px;
+      max-width: 750px;
+      max-height: 450px;
     }
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  #home {
+    .container {
+      flex-direction: column;
+      text-align: center;
+
+      .left {
+        margin-bottom: 30px;
+      }
+
+      .right.terminal {
+        min-width: 300px;
+        height: 400px;
+      }
+    }
+  }
+}
+
+@keyframes type {
+  40%,
+  60% {
+    left: 100%;
+  }
+  100% {
+    left: 0%;
   }
 }
 </style>
