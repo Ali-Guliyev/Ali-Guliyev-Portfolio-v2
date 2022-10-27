d562dc16d88371fd3005b4431b739aac0655828f
Ali Guliyev ⚡ <54935778+Ali-Guliyev@users.noreply.github.com>
2022-10-27 13:29:02 +0300

    code format

--- services/githubService.js
+++ services/githubService.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from "axios"
 
-const userName = "Ali-Guliyev";
-const token = "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";
+const userName = "Ali-Guliyev"
+const token = "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"
 
 const apiClient = axios.create({
   baseURL: `https://api.github.com`,
@@ -10,13 +10,13 @@ const apiClient = axios.create({
     "Content-Type": "application/json",
   },
   login: {},
-});
+})
 
 export default {
   getRepos() {
-    return apiClient.get(`/user/repos`);
+    return apiClient.get(`/user/repos`)
   },
   getRepoByName(repoName) {
-    return apiClient.get(`/repos/${userName}/${repoName}`);
+    return apiClient.get(`/repos/${userName}/${repoName}`)
   },
-};
+}
