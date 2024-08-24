bc29efee1a54e4ca24f9aac42b9ae1eacfb57465
Ali-Quliyev <ali.guliyevoo7@gmail.com>
2024-08-24 22:31:06 +0300

    netlify deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/css/global.css"],
+  components: true,
+  ssr: false,
+  target: 'static',
+  nitro: {
+    preset: 'netlify',
+  },
+  generate: {
+    fallback: true,
+  },
 });
